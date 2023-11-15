7f7fa481cb3cda2e23c8a8d4bcb46f0f857ecc89
Kevindrake <knieves129@gmail.com>
2023-11-14 22:06:51 -0400

    I Separated the Javascript code from the HTML file

--- Widgety/app.js
+++ Widgety/app.js
@@ -1,3 +1,25 @@
-'use strict';
+const apiKey = '0641705d9454b5aea687b2d6f0cda988'; //  API key
+const city = 'Puerto Rico'; //  desired city
 
-console.log('Hello world');
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+
+        console.log(data);
+        if (data.cod === '404') {
+            throw new Error('City not found');
+        }
+
+        const weatherData = 
+                '<p>City: ${data.name}</p>'
+                '<p>Temperature: ${data.main.temp}°C</p>'
+                '<p>Weather: ${data.weather[0].description}</p>';
+
+        document.getElementById('weather-data').innerHTML = weatherData;
+    })
+    .catch((error) => {
+        console.error('Error fetching weather data:', error);
+        document.getElementById('weather-data').innerHTML = 'Error fetching weather data';
+    });
